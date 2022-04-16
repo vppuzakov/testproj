df5c5a77365552fc933f5033b2ccd7815264e886
Vladimir Puzakov <puzakov@tochka.com>
2022-04-16 14:58:11 +0300

    tests: inner fixtures

--- tests/payments/test_payments.py
+++ tests/payments/test_payments.py
@@ -1,17 +1,30 @@
+from decimal import Decimal
 import pytest
 
 from testproj.payments import Account, bill, InsufficientMoney
+
+
+@pytest.fixture
+def account(fake):
+    
+    def inner(name: str = None, amount: Decimal = None):
+        return Account(
+            account=name or fake.pystr(),
+            amount=amount or fake.pyint(),
+        )
+
+    return inner
 
 
 @pytest.mark.parametrize('source_amount, bill_amount', [
     (1000, 200),
     (50, 50),
 ])
-def test_bill_success(fake, source_amount, bill_amount):
+def test_bill_success(fake, account, source_amount, bill_amount):
     destination_amount = fake.pyint()
 
-    source = Account(fake.pystr(), amount=source_amount)
-    destination = Account(fake.pystr(), amount=destination_amount)
+    source = account(amount=source_amount)
+    destination = account(amount=destination_amount)
 
     bill(source, destination, bill_amount)
 
@@ -19,13 +32,13 @@ def test_bill_success(fake, source_amount, bill_amount):
     assert destination.amount == destination_amount + bill_amount
 
 
-def test_insufficient_money(fake):
+def test_insufficient_money(fake, account):
     source_amount = 50
     bill_amount = 200
 
     destination_amount = fake.pyint()
-    source = Account(fake.pystr(), amount=source_amount)
-    destination = Account(fake.pystr(), amount=destination_amount)
+    source = account(amount=source_amount)
+    destination = account(amount=destination_amount)
 
     with pytest.raises(InsufficientMoney):
         bill(source, destination, bill_amount)
